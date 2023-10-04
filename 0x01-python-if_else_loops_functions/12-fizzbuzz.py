97debfb2636f111f8540d90e99c41ab81c6257c2
Obodo David <obododavidsop@gmail.com>
2023-10-04 21:39:38 +0100

    Updated 12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for i in range(101):
-        if i % 3 == 0 or i % 5 == 0:
+    for i in range(1, 101):
+        if (i % 3 == 0 or i % 5 == 0):
             if i % 3 == 0:
                 print("Fizz", end="")
             if i % 5 == 0:
