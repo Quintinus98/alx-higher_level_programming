77650dc4cec3a2eb69d2911a95c15ff927dfe7e0
Obodo David <obododavidsop@gmail.com>
2023-10-05 04:14:51 +0100

    Update

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include "lists.h"
 
 /**
@@ -12,28 +13,40 @@ listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *temp = *head;
 	listint_t *newNode = malloc(sizeof(listint_t));
-	int idx = 0;
+	int idx = 0, flag = 0;
 
-	if (!newNode || !temp)
+	if (!newNode)
 		return (NULL);
-
 	newNode->n = number;
 	newNode->next = NULL;
-
-	while (temp->n < newNode->n)
+	if (*head == NULL)
 	{
-		if (!temp->next)
-			return (NULL);
-		temp = temp->next;
+		*head = newNode;
+		return (*head);
+	}
+	for (; newNode->n > temp->n; temp = temp->next)
+	{
+		if (temp->next == NULL)
+		{
+			flag = 1;
+			break;
+		}
 		idx++;
 	}
-	temp = *head;
-	while (idx != 1)
+	if (!temp->next && flag == 1)
 	{
-		temp = temp->next;
-		idx--;
+		temp->next = newNode;
+		return (temp);
 	}
-
+	temp = *head;
+	if (idx == 0)
+	{
+		newNode->next = *head;
+		*head = newNode;
+		return (newNode);
+	}
+	for (; idx != 1; idx--)
+		temp = temp->next;
 	newNode->next = temp->next;
 	temp->next = newNode;
 	return (newNode);
--- 0x01-python-if_else_loops_functions/13-main.c
+++ 0x01-python-if_else_loops_functions/13-main.c
@@ -25,7 +25,8 @@ int main(void)
 
 	printf("-----------------\n");
 
-	insert_node(&head, 27);
+	insert_node(&head, 2099);
+	insert_node(&head, -7);
 
 	print_listint(head);
 
