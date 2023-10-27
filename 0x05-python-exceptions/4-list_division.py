bc758870a4c330ffdcf25032e9394bfb65c254a7
David <obododavidsop@gmail.com>
2023-10-27 18:29:29 +0100

    added 2 files and empty files

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    n = min(len(my_list_1), len(my_list_2))
+    if list_length > n:
+        print("out of range")
+    new_list = []
+    for i in range(n):
+        try:
+            elem = my_list_1[i]/my_list_2[i]
+        except ZeroDivisionError:
+            elem = 0
+            print("division by 0")
+        except TypeError:
+            elem = 0
+            print("wrong type")
+        finally:
+            new_list.append(elem)
+    for i in range(list_length - n):
+        new_list.append(0)
+    return new_list
