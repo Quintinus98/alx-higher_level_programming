af96ece111c08c7103ca59156974de03fe84af84
David <obododavidsop@gmail.com>
2023-10-30 16:02:51 +0100

    Update

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -6,8 +6,8 @@ class Square:
     """Represents a Square, with a size"""
     def __init__(self, size=0, position=(0, 0)):
         """Initializes the data"""
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
