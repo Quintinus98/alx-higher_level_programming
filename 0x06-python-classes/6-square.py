5aae0c2aa45911bef08c2a25638d66f86bd38815
David <obododavidsop@gmail.com>
2023-10-30 16:09:01 +0100

    Update

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -39,15 +39,15 @@ class Square:
 
     def area(self):
         """Returns the current square area"""
-        return self.__size ** 2
+        return self.size ** 2
 
     def my_print(self):
         """Prints # and spaces"""
-        if self.__size == 0:
+        if self.size == 0:
             print("")
         else:
-            [print("") for _ in range(self.__position[1])]
-            for _ in range(self.__size):
-                [print(" ", end="") for _ in range(self.__position[0])]
-                [print("#", end="") for _ in range(self.__size)]
+            for i in range(self.position[1]):
                 print("")
+            for i in range(self.size):
+                print(" " * self.position[0], end="")
+                print("#" * self.size)
