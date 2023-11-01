3d99a2143abb91cf272c682b2bdabdc070ab1ce0
David <obododavidsop@gmail.com>
2023-11-01 02:26:40 +0100

    Added optional files

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def magic_string(la=[]):
+    la += ["BestSchool"]
+    return (", ".join(la))
---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,7 @@
+#/usr/bin/python3
+"""Locked class"""
+
+
+class LockedClass:
+    """Represents a LockedClass"""
+    __slots__ = ["first_name"]
