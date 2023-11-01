dc2d9df0caa0f8ff1348ffce1d80b6e04ead17b0
David <obododavidsop@gmail.com>
2023-11-01 02:47:43 +0100

    101

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -4,8 +4,8 @@
 
 class LockedClass:
     """
-    Represents a LockedClass - prevents the user from dynamically 
-    creating new instance attributes, except if the new instance 
+    Represents a LockedClass - prevents the user from dynamically
+    creating new instance attributes, except if the new instance
     attribute is called first_name.
     """
 
