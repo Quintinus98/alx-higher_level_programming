329c627d476fd214504c23b88fc10a3eddca9bb4
David <obododavidsop@gmail.com>
2023-11-12 07:30:21 +0100

    1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,13 +4,7 @@
 
 class MyList(list):
     """ Represents a subclass of list """
-    def __init__(self):
-        """Initializes the base class"""
-        super().__init__()
 
     def print_sorted(self):
         """ Prints a sorted list """
         print(sorted(self))
-
-    def __str__(self):
-        return "MyList is subclass of list"
