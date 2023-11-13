6bee1496c7184b9e9436c6741e290dab371de1e4
David <obododavidsop@gmail.com>
2023-11-13 21:00:46 +0100

    0

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """Unit test for py models"""
 import unittest
+from models.base import Base
 
 
-class TestBase(unittest.TestCase):
-    def test_base(self):
-        pass
+class TestBaseMethods(unittest.TestCase):
+    def test_default_id(self):
+        b = Base()
+        self.assertEqual(b.id, 1)
 
 
 if __name__ == '__main__':
