b62eaead5a561f64fbc1aba4f8a5242ab267af2f
David <obododavidsop@gmail.com>
2023-11-14 05:36:41 +0100

    16

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Base Class"""
+import json
 
 
 class Base:
@@ -13,3 +14,19 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        filename = str(cls.__name__) + '.json'
+        list_dict = []
+        if list_objs is not None:
+            for i in range(len(list_objs)):
+                list_dict.append(list_objs[i].to_dictionary())
+        with open(filename, 'w', encoding='utf-8') as a_file:
+            return a_file.write(cls.to_json_string(list_dict))
