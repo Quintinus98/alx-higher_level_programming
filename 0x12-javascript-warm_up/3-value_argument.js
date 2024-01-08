43f2c9868908814d4edbd8155ebf811c7496fb63
David <obododavidsop@gmail.com>
2024-01-08 22:41:21 +0100

    1, 2, 3

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const { argv } = require('node:process');
+
+let state = 0;
+argv.forEach((val, index) => {
+  if (index >= 2) {
+    state = 1;
+    console.log(val);
+  }
+});
+if (state === 0) {
+  console.log('No argument');
+}
