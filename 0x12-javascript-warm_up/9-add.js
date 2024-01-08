115b75ae68daeec9c2c0029f740b94c8f4a16b13
David <obododavidsop@gmail.com>
2024-01-09 00:18:41 +0100

    9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const arg = process.argv;
 
-function add(a, b) {
-	return a + b;
+function add (a, b) {
+  return a + b;
 }
 
-let sum = add(parseInt(arg[2]), parseInt(arg[3]));
-console.log(sum)
+const sum = add(parseInt(arg[2]), parseInt(arg[3]));
+console.log(sum);
