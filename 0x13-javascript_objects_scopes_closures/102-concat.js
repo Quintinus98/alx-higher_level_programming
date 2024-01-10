0002d8fbe043fe283222217478612f058bc86daf
David <obododavidsop@gmail.com>
2024-01-10 04:21:26 +0100

    102

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
-exports.dict = {
-  89: 1,
-  90: 2,
-  91: 1,
-  92: 3,
-  93: 1,
-  94: 2
-};
+const fs = require('fs');
+
+const readfile1 = fs.readFileSync(process.argv[2], 'utf-8');
+const readfile2 = fs.readFileSync(process.argv[3], 'utf-8');
+
+fs.writeFileSync(process.argv[4], readfile1 + readfile2);
