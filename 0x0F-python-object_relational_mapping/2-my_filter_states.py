f16469e430b81165218f6c7e31b8f24b3aeaddf5
David <obododavidsop@gmail.com>
2024-01-12 03:39:08 +0100

    2 Update

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,7 +10,8 @@ def main():
                            passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
 
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name={}".format(sys.argv[4]))
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+                .format(sys.argv[4]))
 
     query_rows = cur.fetchall()
     for row in query_rows:
