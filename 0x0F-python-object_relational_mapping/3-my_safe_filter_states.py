8e370c83ce8d8731923eac9d5b448fd1dfe72bc8
David <obododavidsop@gmail.com>
2024-01-12 03:52:36 +0100

    3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""A script that lists all states with a name starting
+with N (upper N) from the database hbtn_0e_0_usa"""
+import MySQLdb
+import sys
+
+
+def main():
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+
+    cur = conn.cursor()
+    state_searched = sys.argv[4]
+    cur.execute("SELECT * FROM states WHERE name LIKE %s", (state_searched, ))
+
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+
+    cur.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
