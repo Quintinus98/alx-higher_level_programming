7617a70b87ecdb13f407a1b26cf16db51d778eb2
David <obododavidsop@gmail.com>
2024-01-14 23:49:18 +0100

    Updated model state

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -10,6 +10,7 @@ Base = declarative_base(metadata=mymetadata)
 
 
 class State(Base):
+    """Represents a State class that inherits from Base"""
     __tablename__ = 'states'
     id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
