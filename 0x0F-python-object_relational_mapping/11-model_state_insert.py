aba4731ba156e7fdc214e492f61be8f8db98f9e7
David <obododavidsop@gmail.com>
2024-01-15 01:38:03 +0100

    11

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""A script that lists all State objects from the database
+hbtn_0e_6_usa"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    # Connect to the database
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(engine)
+    session = Session()
+    try:
+        session.add(State(name=sys.argv[4]))
+        session.commit()
+    finally:
+        session.close()
