872d52b2f75f9826a615a12e6896f0255bc76958
David <obododavidsop@gmail.com>
2024-01-15 02:17:00 +0100

    14

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ contains the class definition of a City and an instance"""
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from model_state import Base
 
 
