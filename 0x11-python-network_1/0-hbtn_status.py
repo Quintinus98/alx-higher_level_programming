9d20d1e5a96a3def16de9084c4705bba9e331cab
David <obododavidsop@gmail.com>
2024-01-27 20:19:23 +0100

    0 updated

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """A Python script that fetches https://alx-intranet.hbtn.io/status"""
-import urllib.request
 
-req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
-with urllib.request.urlopen(req) as res:
-    content = res.read()
-    print("Body response:")
-    print("    - type: {}".format(type(content)))
-    print("    - content: {}".format(content))
-    print("    - utf8 content: {}".format(content.decode('utf-8')))
+if __name__ == '__main__':
+    import urllib.request
+
+    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
