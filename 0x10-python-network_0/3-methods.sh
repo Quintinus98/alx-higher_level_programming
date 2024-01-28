9ab148f063411834e1c176945fca85ee31ffa1cd
David <obododavidsop@gmail.com>
2024-01-29 00:11:51 +0100

    4

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# A Bash script that sends a DELETE request to the URL
+# A Bash script that displays all HTTP methods the server will accept
 curl -sIL -X "OPTIONS" "$1" | grep -i Allow | awk '{$1=""; print $0}' | sed 's/^ //g'
---
+++ 0x10-python-network_0/4-header.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# A Bash script that updates the header variable X-School-User-Id with the value 98
+curl -sH "X-School-User-Id: 98" "$1"
