a1868ca300f2503e6ed6ec61a6094cc66a42e1fb
David <obododavidsop@gmail.com>
2024-01-28 12:21:01 +0100

    updated 100-github

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -12,9 +12,9 @@ if __name__ == '__main__':
                "X-GitHub-Api-Version": "2022-11-28"}
     url = f'https://api.github.com/repos/{owner}/{repo}/commits'
 
-    r = requests.get(url, headers=headers)
-    for i in range(10):
-        res = r.json()[i].get('commit')
-        sha = res.get('tree').get('sha')
-        author = res.get('author').get('name')
+    r = requests.get(url, headers=headers, params={'per_page': 10})
+    commits = r.json()
+    for commit in commits:
+        sha = commit.get('sha')
+        author = commit['commit']['author']['name']
         print("{}: {}".format(sha, author))
