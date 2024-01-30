81a64cea11bfd0277873ca08f72084dd4cc88c3d
David <obododavidsop@gmail.com>
2024-01-30 05:11:14 +0100

    updated

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A Bash script that sends a JSON POST request to a URL
-curl -H "Content-Type: application/json" -H "Accept: application/json" -d "@$2" "$1"
+curl -sH "Content-Type: application/json" -H "Accept: application/json" -d "@$2" "$1"
