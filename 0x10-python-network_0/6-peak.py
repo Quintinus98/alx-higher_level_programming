5cb78b80f11428fbe2bd8cc29dba29660feac152
David <obododavidsop@gmail.com>
2024-01-30 04:05:33 +0100

    6 updated

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,20 +6,28 @@ def find_peak(list_of_integers):
     """ Retrieves the peak integer in a list of integers """
     if list_of_integers is None or len(list_of_integers) == 0:
         return (None)
-    arr = list_of_integers
+
+    arr = list_of_integers  # name is too long.
     n = len(arr)
-    if n == 1:
+    start = 0
+    end = n - 1
+
+    if start == end:
         return arr[0]
-    for i in range(n):
-        # first element is greater than its only neighbour
-        if i == 0 and arr[i] >= arr[i + 1]:
-            return arr[i]
-        # last element is greater than its only neighbour
-        if i == n - 1 and arr[i] >= arr[i - 1]:
-            return arr[i]
-        # It is greater than its left and right neighbours
-        if arr[i] >= arr[i - 1] and arr[i] >= arr[i + 1]:
-            return arr[i]
+    mid = (start + end) // 2
+
+    # if just 2 elems are remaining, return the greater one.
+    if mid == 0:
+        return arr[mid] if arr[mid] > arr[end] else arr[end]
+
+    if arr[mid] >= arr[mid - 1] and arr[mid] >= arr[mid + 1]:
+        return arr[mid]
+
+    # Peak is in left half
+    if arr[mid - 1] > arr[mid]:
+        return find_peak(arr[:mid])
+    else:
+        return find_peak(arr[mid:])
 
 
 if __name__ == '__main__':
