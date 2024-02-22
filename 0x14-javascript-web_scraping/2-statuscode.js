de7f3164d6941cddc0c4078f9e8640e222cf1457
David <obododavidsop@gmail.com>
2024-02-22 22:21:37 +0100

    2-statuscode.js

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const request = require('request');
+
+const url = process.argv[2];
+
+request(url, (err, res) => {
+  if (err) console.log(err);
+  console.log('code: %d', res.statusCode);
+});
