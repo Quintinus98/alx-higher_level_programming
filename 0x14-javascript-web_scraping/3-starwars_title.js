148c5583aa8163111f7f2c667bafd1fed2048d64
David <obododavidsop@gmail.com>
2024-02-22 22:38:21 +0100

    3-starwars_title.js

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const request = require('request');
+
+const movieId = process.argv[2];
+
+const endpoint = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
+
+request(endpoint, (err, res, body) => {
+  if (err) console.log(err);
+  const data = JSON.parse(body);
+  console.log(data.title);
+});
