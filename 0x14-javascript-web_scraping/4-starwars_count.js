0da62376a9c6ab14859abc32b99bc2ec75fec56c
David <obododavidsop@gmail.com>
2024-02-23 22:57:57 +0100

    4 update

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,18 +2,20 @@
 const request = require('request');
 
 const movieApi = process.argv[2];
-const searchCharacter = 18;
+const searchCharacter = '18';
 let count = 0;
 
 request(movieApi, (err, res, body) => {
   if (err) console.log(err);
-  const data = JSON.parse(body);
-  for (let i = 0; i < data.count; i++) {
-    const characters = data.results[i].characters;
-    for (let j = 0; j < characters.length; j++) {
-      const id = parseInt(characters[j].match(/\d+/)[0]);
-      if (id === searchCharacter) { count += 1; }
+  if (res.statusCode === 200) {
+    const data = JSON.parse(body);
+    for (let i = 0; i < data.count; i++) {
+      const characters = data.results[i].characters;
+      for (let j = 0; j < characters.length; j++) {
+        const id = characters[j].split('/');
+        if (id[id.length - 2] === searchCharacter) { count += 1; }
+      }
     }
+    console.log(count);
   }
-  console.log(count);
 });
---
+++ 0x14-javascript-web_scraping/test.js
@@ -0,0 +1,6 @@
+const url = "http://swapi.co/api/people/18/"
+
+id = url.split('/')
+
+console.log(id[id.length - 2])
+console.log(url)
