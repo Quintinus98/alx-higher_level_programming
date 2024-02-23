6805d8d8d335ff11dba08f695290bb218f176b9c
David <obododavidsop@gmail.com>
2024-02-23 02:03:51 +0100

    updated 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,7 +2,7 @@
 const request = require('request');
 
 const movieApi = process.argv[2];
-const searchCharacter = 'https://swapi-api.alx-tools.com/api/people/18/';
+const searchCharacter = 18;
 let count = 0;
 
 request(movieApi, (err, res, body) => {
@@ -11,7 +11,8 @@ request(movieApi, (err, res, body) => {
   for (let i = 0; i < data.count; i++) {
     const characters = data.results[i].characters;
     for (let j = 0; j < characters.length; j++) {
-      if (characters[j] === searchCharacter) { count += 1; }
+      const id = parseInt(characters[j].match(/\d+/)[0]);
+      if (id === searchCharacter) { count += 1; }
     }
   }
   console.log(count);
