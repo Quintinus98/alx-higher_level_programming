f7585f357a91b1e007e996c0b42745931c0a14a8
David <obododavidsop@gmail.com>
2024-02-23 23:12:16 +0100

    updated 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,18 +2,16 @@
 const request = require('request');
 
 const movieApi = process.argv[2];
-const searchCharacter = '18';
-let count = 0;
 
 request(movieApi, (err, res, body) => {
   if (err) console.log(err);
   if (res.statusCode === 200) {
-    const data = JSON.parse(body);
-    for (let i = 0; i < data.count; i++) {
-      const characters = data.results[i].characters;
-      for (let j = 0; j < characters.length; j++) {
-        const id = characters[j].split('/');
-        if (id[id.length - 2] === searchCharacter) { count += 1; }
+    const data = JSON.parse(body).results;
+    let count = 0;
+    for (const filmId in data) {
+      const character = data[filmId].characters;
+      for (const charId in character) {
+        if (character[charId].includes('18')) { count += 1; }
       }
     }
     console.log(count);
